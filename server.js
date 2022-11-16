6b3e575f930c85747accff1e799110cd24114e72
SantGT5 <gianspf@gmail.com>
2022-11-16 23:06:29 +0100

    resolvers graphql

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ const typesArray = loadFilesSync("**/*", {
   extensions: ["graphql"],
 });
 
-const schema = makeExecutableSchema({
-  typeDefs: typesArray,
+const resolversArray = loadFilesSync("**/*", {
+  extensions: ["resolvers.js"],
 });
 
-const root = {
-  products: require("./products/products.model"),
-  orders: require("./orders/orders.model"),
-};
+const schema = makeExecutableSchema({
+  typeDefs: typesArray,
+  resolvers: resolversArray,
+});
 
 const app = express();
 
@@ -25,7 +25,6 @@ app.use(
   "/graphql",
   graphqlHTTP({
     schema: schema,
-    rootValue: root,
     graphiql: true,
   })
 );
